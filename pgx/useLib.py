ac8b49464d68068929dfa1d93ea085f19f6b3b74
pjalagna <pjalagna@gmail.com>
2021-03-18 03:59:59 -0400

    2021 backup

--- pgx/useLib.py
+++ pgx/useLib.py
@@ -5,7 +5,7 @@ sets up use of ..Lib files
 """
 import os
 M = os.getcwd() # my path
-os.chdir('/Users/PJA/GitHub/Aryc/Lib') 
+os.chdir('/Users/paul/Documents/GitHub/Aryc2019/Lib/') 
 D = os.getcwd() # lib path 
 import sys
 sys.path.append(D)
